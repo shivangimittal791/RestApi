33c1399703c6f94ebc6142f4f34083117caa79b5
Shivangi Mittal <shivangimittal791@gmail.com>
2023-10-09 21:32:06 +0530

    Added project2

---
+++ restapi-v2/src/main/java/com/restapi/restapi/Controller.java
@@ -0,0 +1,13 @@
+package com.restapi.restapi;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
---
+++ restapi/README.md
@@ -0,0 +1,2 @@
+First Project to deploy java springboot restapi to AWS Elastic BeanStalk.
+Teck stack: Java, Springboot
