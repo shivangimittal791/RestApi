2062ac7839d4767d37707e6670beaaba7147dd87
Shivangi Mittal <shivangimittal791@gmail.com>
2023-10-10 22:26:47 +0530

    Updated

--- restapi-v2/src/main/java/com/restapi/restapi/Controller.java
+++ restapi-v2/src/main/java/com/restapi/restapi/Controller.java
@@ -7,6 +7,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Optional;
+
 @Slf4j
 @RestController
 public class Controller {
@@ -20,7 +22,7 @@ public class Controller {
     }
 
     @PostMapping("/insertRecord")
-    public ResponseEntity<ResponseModel> inserRecord(@RequestBody RequestPayloadModel payload){
+    public ResponseEntity<ResponseModel> insertRecord(@RequestBody RequestPayloadModel payload){
 
         log.info("Inside POST"+" "+String.valueOf(payload));
         String username = payload.getUsername();
@@ -32,10 +34,16 @@ public class Controller {
     }
 
     @DeleteMapping("/deleteRecord/{id}")
-    public ResponseEntity<ResponseModel> deleteRecord(@PathVariable int id){
+    public ResponseEntity<ResponseModel> deleteRecord(@PathVariable int id) {
 
-        repository.deleteById(id);
-        return new ResponseEntity<>(ResponseModel.builder().response("Record deleted successfully for id: "+""+id).build(), HttpStatus.OK);
+        Optional<RestEntity> user = repository.findById(id);
+        log.info(String.valueOf(user));
+        if (user.toString() == "Optional.empty") {
+            return new ResponseEntity<>(ResponseModel.builder().response("Record doesn't exist for id: " + "" + id).build(), HttpStatus.NOT_FOUND);
+        } else {
+            repository.deleteById(id);
+            return new ResponseEntity<>(ResponseModel.builder().response("Record deleted successfully for id: " + "" + id).build(), HttpStatus.OK);
+        }
     }
 
 }
--- restapi-v2/src/main/java/com/restapi/restapi/RestEntity.java
+++ restapi-v2/src/main/java/com/restapi/restapi/RestEntity.java
@@ -1,10 +1,7 @@
 package com.restapi.restapi;
 
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Builder;
 
 @Builder
